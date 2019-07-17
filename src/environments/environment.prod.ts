fab86fcf2870db71d85deb8c0618eb8206919dd1
LeonIfran <leonsifran@gmail.com>
2019-07-17 19:43:12 -0300

    dsasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyCbXAuGzQv5aDdDp21K7u1ewSf1yruptCw",
+    apiKey: "",
     authDomain: "segundoparcial-a8ee1.firebaseapp.com",
     databaseURL: "https://segundoparcial-a8ee1.firebaseio.com",
     projectId: "segundoparcial-a8ee1",
     storageBucket: "segundoparcial-a8ee1.appspot.com",
     messagingSenderId: "411841311453",
-    appId: "1:411841311453:web:0e004e5ca97aa9f1"  
+    appId: ""  
   }
 };
