06abaabf4812d63b5dfdb9a1fc433cf7ba3329ed
LeonIfran <leonsifran@gmail.com>
2019-07-17 19:34:20 -0300

    environment modificado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCbXAuGzQv5aDdDp21K7u1ewSf1yruptCw",
+    authDomain: "segundoparcial-a8ee1.firebaseapp.com",
+    databaseURL: "https://segundoparcial-a8ee1.firebaseio.com",
+    projectId: "segundoparcial-a8ee1",
+    storageBucket: "segundoparcial-a8ee1.appspot.com",
+    messagingSenderId: "411841311453",
+    appId: "1:411841311453:web:0e004e5ca97aa9f1"  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCbXAuGzQv5aDdDp21K7u1ewSf1yruptCw",
+    authDomain: "segundoparcial-a8ee1.firebaseapp.com",
+    databaseURL: "https://segundoparcial-a8ee1.firebaseio.com",
+    projectId: "segundoparcial-a8ee1",
+    storageBucket: "segundoparcial-a8ee1.appspot.com",
+    messagingSenderId: "411841311453",
+    appId: "1:411841311453:web:0e004e5ca97aa9f1"  
+  }
 };
 
 /*
